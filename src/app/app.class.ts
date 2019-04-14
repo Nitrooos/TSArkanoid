51d76d72c0b3518c1c5bdea09fad811950218840
Bartosz Kostaniak <bartosz.kostaniak@stxnext.pl>
2019-04-14 19:04:14 +0200

    [ARK-2] Properly update canvas height on resize

--- src/app/app.class.ts
+++ src/app/app.class.ts
@@ -1,5 +1,17 @@
+import { debounce } from 'app/shared/delay.utils';
+
 export class App {
   public run() {
-    return 0;
+    this.updateCanvasHeight();
+    window.addEventListener(
+      'resize',
+      debounce(this.updateCanvasHeight, 100, false),
+    );
+  }
+
+  private updateCanvasHeight() {
+    const canvasElement =
+      document.getElementById('canvas') as HTMLCanvasElement;
+    canvasElement.height = canvasElement.clientWidth;
   }
 }
